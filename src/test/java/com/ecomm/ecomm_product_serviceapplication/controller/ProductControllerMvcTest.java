84907607fb4e1dbcf8cb8398a4766667fcbeaf5e
Sarang <sarang1221@gmail.com>
2025-12-30 14:41:45 +0530

    getProductsById webMVC testcases updated

--- src/test/java/com/ecomm/ecomm_product_serviceapplication/controller/ProductControllerMvcTest.java
+++ src/test/java/com/ecomm/ecomm_product_serviceapplication/controller/ProductControllerMvcTest.java
@@ -3,24 +3,22 @@ package com.ecomm.ecomm_product_serviceapplication.controller;
 import com.ecomm.ecomm_product_serviceapplication.dto.CategoryResponseDto;
 import com.ecomm.ecomm_product_serviceapplication.dto.ProductResponseDto;
 import com.ecomm.ecomm_product_serviceapplication.service.IProductService;
-import com.ecomm.ecomm_product_serviceapplication.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
+@Import(ControllerAdvisor.class)
 class ProductControllerMvcTest {
 
     @MockBean
@@ -33,7 +31,7 @@ class ProductControllerMvcTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void TestGetAllProducts_ReturnsProductsSuccessfully() throws Exception {
+    public void TestGetAllProducts_ReturnsProductsSuccessfully() throws Exception {
         List<ProductResponseDto> mockProducts = List.of(
                 new ProductResponseDto(1L, "productOne", "Test ProductOne Description", 100.0, "http://test-product-one",
                         new CategoryResponseDto(1L, "Category1", "Description1")),
@@ -59,7 +57,20 @@ class ProductControllerMvcTest {
     }
 
     @Test
-    void TestGetProductById_WithValidProductId_ReturnsProductSuccessfully() throws Exception {
+    public void TestGetAllProducts_WhenNoProducts_ReturnsEmptyList() throws Exception {
+        // Arrange
+        when(productService.getAllProducts()).thenReturn(List.of());
+
+        // Act && Assert
+        mockMvc.perform(get("/product"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isEmpty());
+
+        verify(productService, times(1)).getAllProducts();
+    }
+
+    @Test
+    public void TestGetProductById_WithValidProductId_ReturnsProductSuccessfully() throws Exception {
         // Arrange
         ProductResponseDto mockProduct = new ProductResponseDto(1L, "productOne", "Test ProductOne Description", 100.0, "http://test-product-one",
                 new CategoryResponseDto(1L, "Category1", "Description1"));
@@ -77,6 +88,19 @@ class ProductControllerMvcTest {
                 .andExpect(jsonPath("$.category.name").value("Category1"));
 
         verify(productService, times(1)).getProductById(1L);
+    }
+
+    @Test
+    public void TestGetProductById_WithInvalidProductId_ThrowsProductNotFoundException() throws Exception {
+        // Arrange
+        when(productService.getProductById(3L)).thenReturn(null);
+
+        // Act && Assert
+        mockMvc.perform(get("/product/3"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Invalid Product ID"));
+
+        verify(productService, times(1)).getProductById(3L);
     }
 
     @Test
